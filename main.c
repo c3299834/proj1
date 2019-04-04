7d596b29bb82f2b9c585249a776f4554f4d2b04a
Andrew Heckendorf <andrew.heckendorf@uon.edu.au>
2019-04-04 05:23:43 +0000

    Testing string and how it is carried over to functions and their return values

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <math.h>
+#include <strings.h>
+
+char setValue(char *str, int TOTAL);
 
 int main() {
-  
-  return 0;
+    char str[] = "abcdefghijklmnopqrstuvwxyz"; //Defines the alphabet string
+    int /*i = 0,*/ TOTAL = 26; //Defining a counting variable and the string size as TOTAL (includes \0)
+    
+    setValue(str, TOTAL);
+    
+    /*for(i = 0; i < TOTAL; i++) {
+        printf("%d: %c\n", i, str[i] );
+    }*/
+
+    
+
+    return 0;
+}
+
+// Function which changes each string segment and gives it a value
+char setValue(char *str, int TOTAL) {
+    int u = 0; //Define a counter
+    //printf("Hello\n"); //Testing whether code has passed this point
+    
+    for(u = 0; u < TOTAL; u++) {
+        str[u] = u;
+        printf("%d", str[u] );
+        //printf("%d: Hi\n", u); //Testing whether code has passed this point
+    }
+    return 0;
 }
