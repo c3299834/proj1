c4bd85563f4d8efabebf3dc14b92ea6320a2c365
Andrew Heckendorf <andrew.heckendorf@uon.edu.au>
2019-04-10 23:55:56 +0000

    Reads phrase and encrypts it given a rotation

--- main.c
+++ main.c
@@ -11,15 +11,17 @@ int main() {
     //printf("What is the message?\n"); //Asks what the message is
     scanf("%[^\n]s", str); //Reads a statement which contains a space
     
-    //printf("What is the rotation?\n");
+    //printf("What is the rotation?\n"); //Asks what the rotation is
     scanf("%d", &rotation); //Reads the rotation
     
     //printf("%d\n", rotation); //Tests whether the rotation has been read correctly 
     //printf("%s\n", str); //Tests whether it has read the statement correctly
     
     for(i = 0; str[i] != 0; i++) {
-        newletter = str[i] + rotation;
-        printf("%c", str[newletter]);
+        //printf("%d ", str[i]); //Prints ASCII numbers of the string
+        newletter = str[i] - rotation; //Calculates new letter after rotation has been taken into account
+        //printf("%d ", newletter); //Prints the new letter ASCII number
+        printf("%c", newletter); //Prints the encrypted phrase with the specified rotation
     }
     return 0;
 }
