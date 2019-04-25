d03991e0162661e7ca9c0f6b39fb276eb27f2437
Andrew Heckendorf <andrew.heckendorf@uon.edu.au>
2019-04-25 05:17:59 +0000

    Menu complete, linked to functions and asks for user input correctly

--- main.c
+++ main.c
@@ -4,17 +4,17 @@
 #include <string.h>
 #include <ctype.h>
 
-//int DecryptRotationKey(char str[1024], int newletter);
+int DecryptRotationKey(char str[1024], int newletter);
 //int DecryptRotationNoKey(char str[1024], int newletter);
-//int EncryptRotationKey(char str[1024], int newletter);
-//int EncryptSubKey(char str[1024], char key[26], int newletter, char alphabet[26], char alphabetcap[26]);
-//int DecryptSubKey(char str[1024], char key[26], int newletter, char alphabet[26], char alphabetcap[26]);
+int EncryptRotationKey(char str[1024], int newletter);
+int EncryptSubKey(char str[1024], char key[26], int newletter, char alphabet[26], char alphabetcap[26]);
+int DecryptSubKey(char str[1024], char key[26], int newletter, char alphabet[26], char alphabetcap[26]);
 
 int main() {
     char str[1024]; //String where the message will be stored
     int newletter = 0, option = 1, varDefault = 0;
     char space;
-    char key[1024];
+    //char key[1024];
     char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
     char alphabetcap[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     char key[26] = "mklijrtnadfhzxwvuyqbcegops";
@@ -35,10 +35,10 @@ int main() {
     do {
         varDefault = 0;
         switch(option) {
-            case 1: printf("Hello\n");/*EncryptRotationKey(str, newletter);*/ break;
-            case 2: printf("Hi\n");/*DecryptRotationKey(str, newletter);*/ break;
-            case 3: printf("Hey\n");/*EncryptSubKey(str, key, newletter, alphabet, alphabetcap);*/ break;
-            case 4: printf("Hu\n");/*DecryptSubKey(str, key, newletter, alphabet, alphabetcap);*/ break;
+            case 1: EncryptRotationKey(str, newletter); break;
+            case 2: DecryptRotationKey(str, newletter); break;
+            case 3: EncryptSubKey(str, key, newletter, alphabet, alphabetcap); break;
+            case 4: DecryptSubKey(str, key, newletter, alphabet, alphabetcap); break;
             //case 5:
             //case 6:
             default: printf("Error, choose a valid option.\n");
@@ -63,10 +63,12 @@ int main() {
 int DecryptRotationKey(char str[1024], int newletter) {
     int rotation, i;
     
-    //printf("What is the message?\n"); //Asks what the message is
-    scanf("%[^\n]s", str); //Reads a statement which contains a space
+    printf("What is the message?\n"); //Asks what the message is
+    fflush(stdout);
+    scanf(" %[^\n]s", str); //Reads a statement which contains a space
     
-    //printf("What is the rotation?\n"); //Asks what the rotation is
+    printf("What is the rotation?\n"); //Asks what the rotation is
+    fflush(stdout);
     scanf("%d", &rotation); //Reads the rotation
     
     //printf("%d\n", rotation); //Tests whether the rotation has been read correctly 
@@ -88,8 +90,11 @@ int DecryptRotationKey(char str[1024], int newletter) {
         else if(str[i] == 32) {
             newletter = 32;
         }
-        else {
+        else if((str[i] <= 122 && str[i] >= 97) || (str[i] >= 65 && str[i] <= 90)){
             newletter = str[i] - rotation; //Calculates new letter after rotation has been taken into account
+        }
+        else {
+            continue;
         }
         
         str[i] = newletter;
@@ -145,12 +150,14 @@ int DecryptRotationKey(char str[1024], int newletter) {
 }*/
 
 int EncryptRotationKey(char str[1024], int newletter) {
-    int rotation, i;
+    int rotation = 1, i = 0;
     
-    //printf("What is the message?\n"); //Asks what the message is
-    scanf("%[^\n]s", str); //Reads a statement which contains a space
+    printf("What is the message?\n"); //Asks what the message is
+    fflush(stdout);
+    scanf(" %[^\n]s", str); //Reads a statement which contains a space
     
-    //printf("What is the rotation?\n"); //Asks what the rotation is
+    printf("What is the rotation?\n"); //Asks what the rotation is
+    fflush(stdout);
     scanf("%d", &rotation); //Reads the rotation
     
     //printf("%d\n", rotation); //Tests whether the rotation has been read correctly 
@@ -172,8 +179,11 @@ int EncryptRotationKey(char str[1024], int newletter) {
         else if(str[i] == 32) {
             newletter = 32;
         }
-        else {
+        else if((str[i] <= 122 && str[i] >= 97) || (str[i] >= 65 && str[i] <= 90)){
             newletter = str[i] + rotation; //Calculates new letter after rotation has been taken into account
+        }
+        else {
+            continue;
         }
         
         str[i] = newletter;
@@ -186,10 +196,10 @@ int EncryptRotationKey(char str[1024], int newletter) {
 int EncryptSubKey(char str[1024], char key[26], int newletter, char alphabet[26], char alphabetcap[26]) {
     int i, j, a;
     
-    scanf("%[^\n]s", str);
-    //printf("ok");
+    printf("What is the message?\n"); //Asks what the message is
+    fflush(stdout);
+    scanf(" %[^\n]s", str);
     //scanf("%[^\n]s", key);
-    //printf("what");
     for(i = 0; str[i] != 0; i++) {
         if(str[i] <= 122 && str[i] >= 97) {
             for(j = 0; alphabet[j] != 0; j++) {
@@ -220,10 +230,10 @@ int EncryptSubKey(char str[1024], char key[26], int newletter, char alphabet[26]
 int DecryptSubKey(char str[1024], char key[26], int newletter, char alphabet[26], char alphabetcap[26]) {
     int i, j, a;
     
-    scanf("%[^\n]s", str);
-    //printf("ok");
+    printf("What is the message?\n"); //Asks what the message is
+    fflush(stdout);
+    scanf(" %[^\n]s", str);
     //scanf("%[^\n]s", key);
-    //printf("what");
     for(i = 0; str[i] != 0; i++) {
         if(str[i] <= 122 && str[i] >= 97) {
             for(j = 0; key[j] != 0; j++) {
