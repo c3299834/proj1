479d755133847f79100126edb6374f512f72441c
Andrew Heckendorf <andrew.heckendorf@uon.edu.au>
2019-04-29 04:04:52 +0000

    Still having problems with file io

--- main.c
+++ main.c
@@ -49,9 +49,10 @@ int main() {
         else {
             fscanf(textfile, "%[^\n]s", str); //Otherwise, the file is scanned and it's contents stored in the string 'str'
         }
+        //fclose(textfile);
         
     FILE *textfile2; //Defines the pointer textfile2 as a file
-    textfile2 = fopen("textout", "a"); //Opens the file textout and stores it in the variable textfile2(Encrypted or decrypted data is written to this file)
+    textfile2 = fopen("text2", "a"); //Opens the file textout and stores it in the variable textfile2(Encrypted or decrypted data is written to this file)
 
     /*
      * Following is the user-friendly menu, which asks the user to choose the function the program is to perform. The section contains a 
@@ -187,51 +188,6 @@ int DecryptRotationKey(char str[1024], int newletter) {
     return 0; //Terminates the program as it has completed its objective
 }
 
-/*int DecryptRotationNoKey(char str[1024], int newletter) {
-    int rotation, i, a = 0;
-    
-    //printf("What is the message?\n"); //Asks what the message is
-    scanf("%[^\n]s", str); //Reads a statement which contains a space
-    
-    //printf("What is the rotation?\n"); //Asks what the rotation is
-    //scanf("%d", &rotation); //Reads the rotation
-    
-    //printf("%d\n", rotation); //Tests whether the rotation has been read correctly 
-    //printf("%s\n", str); //Tests whether it has read the statement correctly
-    while(a == 0) {
-        for(i = 0; str[i] != 0; i++) {
-            //printf("%d ", str[i]); //Prints ASCII numbers of the string
-            //printf("%d", i);
-            if(str[i] - rotation < 65 && str[i] >= 65 && str[i] <= 90) {
-                newletter = str[i] - rotation;
-                newletter = 65 - newletter;
-                newletter = 91 - newletter;
-            }
-            else if(str[i] - rotation > 90 && str[i] - rotation < 97 && str[i] >= 97 && str[i] <= 122) {
-                newletter = str[i] - rotation;
-                newletter = 97 - newletter;
-                newletter = 123 - newletter;
-            }
-            else if(str[i] == 32) {
-                newletter = 32;
-            }
-            else {
-                newletter = str[i] - rotation; //Calculates new letter after rotation has been taken into account
-            }
-        
-            str[i] = newletter;
-        
-        }
-        dictionary("parameters");
-        if(dictionary() == 1) {
-            
-        }
-    }
-
-    printf("%s\n", str); //Prints the encrypted phrase with the specified rotation
-    return 0;
-}*/
-
 /*
  * EncryptRotationKey is the first option provided to the user, it encrypts text using a given key.
  * Each value in the string(which is a letter) is increased by the specified rotation to encrypt the given message.
@@ -358,7 +314,7 @@ int EncryptSubKey(char str[1024], char key[26], int newletter, char alphabet[26]
     }
     printf("%s\n", str); //Prints the entire encrypted phrase with the specified rotation
     fprintf(textfile2, "%s\n", str);
-    //fclose(textfile2);
+    fclose(textfile2);
     return 0; //Terminates the program as it has completed its objective
 }
 
